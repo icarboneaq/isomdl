3f4a428870bdabc819f6004b9dcbb4a125993160
Ian Carbone <ian@affinitiquest.io>
2025-03-05 14:53:49 -0500

    Debugging.

--- src/presentation/reader.rs
+++ src/presentation/reader.rs
@@ -206,11 +206,13 @@ impl SessionManager {
             &e_reader_key_private.into(),
         )
         .context("failed to derive shared session secret")?;
-        let a = shared_secret.as_bytes().as_slice();
+        let a = shared_secret.raw_secret_bytes().as_slice();
         let mut shared_secret_hex_string = String::with_capacity(a.len() * 2);
         for &b in a {
             write!(&mut f, "{:02x}", b).unwrap();
         }
+        shared_secret_hex_string
+
         let session_transcript = SessionTranscript180135(
             device_engagement_bytes,
             e_reader_key_public.clone(),
